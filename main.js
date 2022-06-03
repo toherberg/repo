e1d6b7eac976834e147251ee4ab37c548ad204e8
anymacstore <tth4ik@gmail.com>
2022-06-03 15:43:57 +0300

    fetched data used to fill table

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 async function loadIntoTable(url, table) {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
+    fetch(url).then((data) => {
+        return data.json();
+    }).then((objectData) => {
+        let tableData = "";
+        objectData.map((values) => {
+            tableData += `<tr>
+            <td>${values.country}</td>
+            <td>${values.todayCases}</td>
+            <td>${values.cases}</td>
+            <td>${values.deaths}</td>
+            <td>${values.recovered}</td>
+            </tr>`
+            document.getElementById("table_body").innerHTML = tableData;
+        })
+    })
 
 }
 
-loadIntoTable("https://corona.lmao.ninja/v2/countries?yesterday=&sort=", document.querySelector("table"));
+loadIntoTable("https://corona.lmao.ninja/v2/countries?yesterday=&sort=todayCases", document.querySelector("table"));
